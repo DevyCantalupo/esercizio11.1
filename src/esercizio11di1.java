92f76e8666dcf101dd6316a09334b7c2072dc396
Devy Franco Cantalupo <devycantalupo@outlook.com>
2023-10-16 09:28:08 +0200

    First Commit

---
+++ src/esercizio11di1.java
@@ -0,0 +1,10 @@
+public class esercizio11di1 {
+    public static void moltiplicazione(int x){
+        for(int i = 0; i <= 10; i++){
+            System.out.println(x + " x " + i + " = " + (x * i));
+        }
+    }
+    public static void main(String[] args) {
+            moltiplicazione(9);
+    }
+}
